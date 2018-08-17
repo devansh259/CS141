7a756d04364c5360a9150cd257cf18a7cf4c3b68
devansh259 <42205065+devansh259@users.noreply.github.com>
2018-08-17 05:24:20 +0000

    lab 3 q2

---
+++ lab3_q2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=1,b=2;
+float c=1.5,d=3.5;
+cout<<"sum of a,b="<<a+b<<endl;
+cout<<"sum of c,d="<<c+d<<endl;
+cout<<"product of a,b="<<a*b<<endl;
+cout<<"product of c,d="<<c*d<<endl;
+cout<<"a-b="<<a-b<<endl;
+cout<<"c-d="<<c-d<<endl;
+cout<<"division of a by b="<<a/b<<endl;
+cout<<"division of c by d="<<c/d<<endl;
+return 1;
+}
