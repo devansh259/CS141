8e8718ad862cdc738f26707c590ad00d4f986daf
devansh259 <42205065+devansh259@users.noreply.github.com>
2018-08-22 00:36:34 -0400

    lab4 q2

---
+++ lab4_q2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int temp;
+cout<<"enter temprature in celsius"<<endl;
+cin>>temp;
+cout<<"temprature in fahrenhite="<<temp*1.8+32<<endl;
+
+return 1312312;
+}
