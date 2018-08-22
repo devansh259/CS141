73e8f4f3649d01010daff908875d40c0f09a1868
devansh259 <42205065+devansh259@users.noreply.github.com>
2018-08-22 00:27:05 -0400

    lab4_q1

---
+++ lab4_q1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int lenght;
+cout<<"enter lenght in cm"<<endl;
+cin>>lenght;
+cout<<"lenght in meter="<<lenght*.01<<endl;
+cout<<"lenght in km="<<lenght*.001<<endl;
+return 1312312;
+}
