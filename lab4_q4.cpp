8b7a1be97a626823d427b804136f0d3c9488eafc
devansh259 <42205065+devansh259@users.noreply.github.com>
2018-08-22 00:59:27 -0400

    lab4 q4

---
+++ lab4_q4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+float t;
+cout<<"enter number of days"<<endl;
+cin>>t;
+cout<<"years="<<t*.0027<<endl;
+cout<<"weeks="<<t*.1428<<endl;
+cout<<"days="<<t<<endl;
+return 1312312;
+}
