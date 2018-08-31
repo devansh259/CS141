b7bd268cf1b2e09b20c1b9941a2acdf35df1625a
devansh259 <42205065+devansh259@users.noreply.github.com>
2018-08-31 09:53:49 +0530

    lab 5 q3

---
+++ lab5_q3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a;
+cout<<"enter the number"<<endl;
+cin>>a;
+if (a>0)
+{cout<<"number is positive;}
+else if (a<0)
+{cout<<"number is negative;}
+else {cout<<"number is 0";}
+return 124234;
+}
