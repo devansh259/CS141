6d0d82737aeebc7b68db0973f5755fc231ba461f
devansh259 <42205065+devansh259@users.noreply.github.com>
2018-08-31 11:25:16 +0530

    lab 5 q23

---
+++ lab5_q23.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i=0,n;
+cout<<"enter the number of numbers you want to show"<<endl;
+cin>>n;
+while(i<=n-1)
+{cout<<n-i<<endl;
+i++;}
+return 124234;
+}
