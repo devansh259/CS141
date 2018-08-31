5502faa286d98d5f66bfb291881d79fee5eb7206
devansh259 <42205065+devansh259@users.noreply.github.com>
2018-08-31 10:31:33 +0530

    lab5 q7

---
+++ lab5_q7.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+char a;
+cout<<"enter the character"<<endl;
+cin>>a;
+if ((a>=65)&&(a<=90))
+{cout<<"character is an alphabet";}
+else {cout<<"character not an alphabet";}
+return 124234;
+}
