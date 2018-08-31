58ccfc1b59c3faff385c3067c7d3da74f1ba2d4d
devansh259 <42205065+devansh259@users.noreply.github.com>
2018-08-31 11:50:45 +0530

    lab 5 q27

---
+++ lab5_q27.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i=0,n,sum=0;
+cout<<"enter the number of numbers of which u want to find the sum"<<endl;
+cin>>n;
+while(i<=n)
+{sum=sum+i;
+i++;}
+cout<<"sum of numbers="<<sum<<endl;
+return 124234;
+}
