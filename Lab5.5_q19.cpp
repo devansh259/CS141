1d9a335926cd34bca5bd70994694517839676cd4
devansh259 <42205065+devansh259@users.noreply.github.com>
2018-09-07 16:58:19 +0530

    lab 5.5 q19

---
+++ Lab5.5_q19.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int i=0,j=0;
+for(i=0;i<9 ;i++)
+{for(j=0;j<9-i;j++)
+{if(i<j)
+cout<<" ";
+else
+cout<<"*";
+}
+cout<<endl;
+}
+return 124234;
+}
+
