292ce0ad2a9c7d9fef54b6cf0a431f536a5cfe72
devansh259 <42205065+devansh259@users.noreply.github.com>
2018-11-18 15:24:51 +0530

    lab 10 q2

--- lab10q_2.cpp
+++ lab10q_2.cpp
@@ -3,9 +3,9 @@ using namespace std;
 class Box 
 {
    public:
-	double length;//data memeber to store the length of a box
-        double breadth;//data memeber to store the breadth of a box
-        double height;//data member to store the height of a box
+	double length;
+        double breadth;
+        double height;
         //member functions
         double getVolume(void)
 	{
@@ -24,23 +24,22 @@ class Box
  	        height = hei;
 	}
 };
-// Main function for the program
+
 int main() 
 {
-   Box Box1;//an object Box1 of type Box is declared
-   Box Box2;//an object Box2 of type Box is declared
-   double volume = 0.0;//variable to store the volume of a box
-   //specifications of Box1
+   Box Box1;
+   Box Box2;
+   double volume = 0.0;
+   
    Box1.setLength(6.0); 
    Box1.setBreadth(7.0); 
    Box1.setHeight(5.0);
-   //specifications of Box2
    Box2.setLength(12.0); 
    Box2.setBreadth(13.0); 
    Box2.setHeight(10.0);
-   volume=Box1.getVolume();//volume of Box1 is determined
-   cout<<"Volume of Box1 is "<<volume<<"cubic units\n";//and printed
-   volume=Box2.getVolume();//volume of Box2 is determined
-   cout<<"Volume of Box2 is "<<volume<<"cubic units\n";//and printed
+   volume=Box1.getVolume();
+   cout<<"Volume of Box1 is "<<volume<<"cubic units\n";
+   volume=Box2.getVolume();
+   cout<<"Volume of Box2 is "<<volume<<"cubic units\n";
    return 0;
 }
